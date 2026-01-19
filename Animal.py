e20f73c1779bad0654ed5da8cefada88645318c4
Breno Pereira <breno.pereirapc@gmail.com>
2026-01-19 18:54:04 -0300

    Create Animal.py
    Estudo para herança

---
+++ Animal.py
@@ -0,0 +1,18 @@
+class Animal:
+    def __init__(self, nome, espécie):
+        self.nome = nome
+        self.espécie = espécie
+        
+    def comer(self):
+        return f"{self.nome} está comendo."
+    
+    def dormir(self):
+        return f"{self.nome} está dormindo."
+    
+    def fazer_som(self):
+        return f"{self.nome} está fazendo um som."
+    
+
+    
+    
+     
